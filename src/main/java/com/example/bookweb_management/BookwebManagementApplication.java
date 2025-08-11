ee48e7bfe84c244b9be132a905eba3a16f63960d
Vu Quyet <vuquyet.dev@gmail.com>
2025-08-11 13:28:17 +0700

    New Project

---
+++ src/main/java/com/example/bookweb_management/BookwebManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookweb_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwebManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwebManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookweb_management/BookwebManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookweb_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwebManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
